c903c24ad8808019a965968110ab3f2b64dcc320
JustWaveThings <flove101@gmail.com>
2022-06-18 12:57:11 -0400

    Create and insert elements into DOM with JS started

--- dom.js
+++ dom.js
@@ -1,1 +1,42 @@
-div.textcontent = ' hello world';
+const body = document.getElementsByTagName('#body');
+
+// testing out things
+// console.log('body by tagname is: ',body);
+// console.log("body by tagname siblings: ", body.nextSibling);
+// console.log("body by element siblings: ", body.nextElementSibling);
+
+
+// create needed elements for exercise 
+const div = document.createElement('div');
+const para = document.createElement('p');
+const blueH3 = document.createElement('h3');
+const h1 = document.createElement('h1');
+const para2 = document.createElement('p');
+
+// adding text to the elements 
+
+para.textContent = "Hey I'm red!";
+blueH3.textContent = "I'm a blue h3!";
+h1.textContent = "I'm in a div.";
+para2.textContent = "ME TOO!";
+
+
+// placing elements in DOM in correct place
+document.body.appendChild(div);
+document.body.appendChild(para);
+document.body.appendChild(blueH3);
+
+
+// stylizing elements via JS 
+
+para.setAttribute('style', 'color: red;');
+blueH3.setAttribute('style', 'color: blue;');
+div.setAttribute('id', 'container'); 
+
+
+// put h1 and para2 in container
+
+const container = document.querySelector('#container');
+
+container.appendChild(h1);
+container.appendChild(para2);
